dc00a51d32cd5716b4e0d44876a77b3fe7189511
AlekhAvinash <mcdanroy@gmail.com>
2021-10-28 20:16:59 +0530

    update 2.0

--- app/app.py
+++ app/app.py
@@ -1,30 +1,33 @@
-
 #!/usr/bin/python3
 
 from flask import Flask, render_template, request
-import pickle
+from pickle import load
 
-PR = pickle.load(open('static/src/Primes', 'rb'))
-mn, mx = min(PR), max(PR)
 app = Flask(__name__, template_folder = 'template')
 
-def miller_rabin(n, b):
-	basis = PR[:64]
-	# assert n not 2 or 3
-	if n == 2 or n == 3:
-		return True
+PR = load(open('static/src/Primes', 'rb'))
+mx = max(PR)
 
+def miller_rabin(n, k=100):
+	if n in range(2, mx):
+		if n in PR:
+			return True
+		else:
+			return False
+	
 	#assert n not multiple of 2
 	if n % 2 == 0:
 		return False
+
+	k = min(n.bit_length()//10, k)
 
 	# rabin-miller test
 	r, s = 0, n - 1
 	while s % 2 == 0:
 		r += 1
 		s //= 2
-	for b in basis:
-		x = pow(b, s, n)
+	for b in range(k):
+		x = pow(choice(PR), s, n)
 		if x == 1 or x == n - 1:
 			continue
 		for _ in range(r - 1):
@@ -37,13 +40,16 @@ def miller_rabin(n, b):
 
 @app.route('/', methods = ["GET", "POST"])
 def home():
+	out = ""
 	if request.method == "POST":
-		val = int(request.form.get('num'))
-		if (val < mx and val in PR) or (val > mx and miller_rabin(n, 64)):
-			out = 'Prime!'
-		else:
-			out = 'Not a Prime :('
-		return render_template('index.html', text=out)
-	return render_template('index.html')
+		try:
+			val = int(request.form.get('num'))
+			if miller_rabin(val):
+				out = 'Prime!'
+			else:
+				out = 'Not a Prime :('
+		except:
+			out = "Invalid Input!"
+	return render_template('index.html', text=out)
 
 app.run()
